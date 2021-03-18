b0e8126acf1acabe5fa452a57aaa294532b42500
daehyeon-digiPen <65066421+daehyeonkim-digipen@users.noreply.github.com>
2021-03-18 21:14:01 +0900

    Update Logger.cpp

--- CS230/Engine/Logger.cpp
+++ CS230/Engine/Logger.cpp
@@ -22,40 +22,23 @@ CS230::Logger::~Logger() {
 }
 
 void CS230::Logger::Log(CS230::Logger::Severity severity, std::string message) {
-	switch(severity)
+	if(severity >= minLevel)
 	{
+		switch (severity)
+		{
 		case Severity::Error:
-			std::cout << "Error" << '\t' << message << std::endl;
+			std::cout << "Error";
 			break;
 		case Severity::Event:
-			switch (minLevel)
-			{
-			case Severity::Error: break;
-				default:
-					std::cout << "Event" << '\t' << message << std::endl;
-					break;
-			}
+			std::cout << "Event";
 			break;
 		case Severity::Debug:
-			switch (minLevel)
-			{
-			case Severity::Error: break;
-			case Severity::Event: break;
-			default:
-				std::cout << "Debug" << '\t' << message << std::endl;
-				break;
-			}
+			std::cout << "Debug";
 			break;
 		case Severity::Verbose:
-			switch (minLevel)
-			{
-			case Severity::Verbose: 
-				std::cout << "Verb" << '\t' << message << std::endl;
-				break;
-			default:
-				break;
-			}
+			std::cout << "Verb";
 			break;
+		}
+		std::cout << '\t' << message << std::endl;
 	}
 }
-
